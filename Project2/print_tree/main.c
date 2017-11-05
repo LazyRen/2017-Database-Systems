538bb0dfef33803df7c36fc31bd87318a52a6d63
2014004893 <prangia0ren@gmail.com>
2017-11-05 16:19:43 +0900

    Catched minor memory leak

--- Project2/print_tree/main.c
+++ Project2/print_tree/main.c
@@ -2,15 +2,22 @@
 
 int main(int argc, char ** argv)
 {
-	char path[100];
+	char *path;
+	path = (char*)malloc(100);
 	int temp = -1;
+	if (sizeof(header_page) != 4096 || sizeof(page) != 4096)
+	{
+		printf("header_page or page's size is not 4096\n");
+		exit(EXIT_FAILURE);
+	}
+
 	while (temp != 0) {
-		if (argc >= 2)
+		if (argc >= 2) {
 			temp = open_db(argv[1]);
+			argc = 1;
+		}
 		else {
-			printf("input path of db: ");
-			scanf("%s", path);
-			temp = open_db(path);
+			printf("Give DB file location as an second argument in terminal\n");
 		}	
 	}
 	print_tree();
